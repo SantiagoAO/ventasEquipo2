14f1b2a327fd86fea0b9cf99a05d31e6b2fc957e
SantiagoAO <santiagoao2@hotmail.com>
2025-04-06 22:54:32 -0600

    Sign Up corregido

--- php/sign_up.php
+++ php/sign_up.php
@@ -3,21 +3,33 @@ session_start();
 require_once '../php/conexion.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nombre = $_POST['nombre'];
     $usuario = $_POST['usuario'];
-    $contraseña = $_POST['contraseña'];
+    $password = $_POST['contraseña'];
 
     try {
-        $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE usuario = :usuario");
+        // Verificar si el usuario ya existe
+        $stmt = $pdo->prepare("SELECT COUNT(*) FROM usuarios WHERE usuario = :usuario");
         $stmt->execute(['usuario' => $usuario]);
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        $count = $stmt->fetchColumn();
 
-        if ($user && password_verify($contraseña, $user['contraseña'])) {
-            $_SESSION['usuario_id'] = $user['id'];
-            header('Location: catalogo.php');
+        if ($count > 0) {
+            echo "<script>alert('El usuario ya existe, intenta otro.'); window.location.href='../sign_up.html';</script>";
             exit;
-        } else {
-            echo "<script>alert('Usuario o contraseña incorrectos');</script>";
         }
+        
+        $stmt = $pdo->query("SELECT MAX(id) FROM usuarios");
+        $max_id = $stmt->fetchColumn();
+
+        $id = $max_id + 1;
+
+        $contraseña = password_hash($password, PASSWORD_DEFAULT);
+        $stmt = $pdo->prepare("INSERT INTO usuarios (id, nombre, usuario, contraseña) VALUES (:id, :nombre, :usuario, :contrasena)");
+        $stmt->execute(['id' => $id, 'nombre' => $nombre, 'usuario' => $usuario, 'contrasena' => $contraseña]);
+
+        echo "<script>alert('Usuario creado correctamente.'); window.location.href='../index.html';</script>";
+        exit;
+
     } catch (PDOException $e) {
         die("Error: " . $e->getMessage());
     }
